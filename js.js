ee12b1c95aee8989b8a579a8acb3094d61938bef
sodizdaroglu <americanlaz@gmail.com>
2020-08-19 10:14:15 -0400

    joke

---
+++ js.js
@@ -0,0 +1,28 @@
+//https://github.com/15Dkatz/official_joke_api
+let setup = document.querySelector('.para1');
+let punchline = document.querySelector('.para2');
+let button = document.querySelector('#button');
+button.addEventListener('click', () => {
+	setup.innerHTML = '';
+	punchline.innerHTML = '';
+	
+	// Fetch API CALL
+	fetch('https://official-joke-api.appspot.com/jokes/ten'
+    )
+	.then(res => res.json())
+	.then(data => {
+		
+		let randomNum = Math.floor((Math.random() * data.length));
+			console.log(data[randomNum]);
+			let para1 = document.createTextNode(
+			`Setup: ${data[randomNum].setup}`
+			);
+			let para2 = document.createTextNode(`Punchline: ${data[randomNum].punchline}`);
+			 setup.appendChild(para1);
+			punchline.appendChild(para2);
+	})
+	.catch(error => console.log(error));
+});
+
+
+
